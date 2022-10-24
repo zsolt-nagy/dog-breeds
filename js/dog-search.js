a5465953bc6e330fcefd77ca579bdf77057a6f70
Zsolt Nagy <info@zsoltnagy.eu>
2022-10-25 01:36:53 +0200

    dog list rendering

--- js/dog-search.js
+++ js/dog-search.js
@@ -2,6 +2,8 @@
 let form = document.querySelector('.js-form');
 // select is a handle of the dropdown list inside the form element.
 let select = document.querySelector('.js-dog-breed-chooser');
+// a container, where the results are rendered
+let resultsContainer = document.querySelector('.js-dogs-container');
 
 // Submit event handler function. 
 function formSubmitted(event) {
@@ -18,7 +20,19 @@ function formSubmitted(event) {
     // Fetch and console log data
     fetch(apiUrl)
         .then(data => data.json())
-        .then(response => console.log(response)); // or: .then(console.log);
+        .then(renderDogs); 
+}
+
+function renderDogs(response) {
+    let html = '<ul class="dog-list">';
+    for (let url of response.message) {
+        html += `
+            <li class="dog-item">
+                <img class="dog-image" src="${url}" alt="Image of a dog (query result)." />
+            </li>`;
+    }
+    html += '</ul>';
+    resultsContainer.innerHTML = html;
 }
 
 // The below line attaches an event handler function to the submit event
